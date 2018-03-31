956f02e6dc1eb4b7dca6dc48ee9d62d1a9cb0131
eennaa <ukradenitrenutak@gmail.com>
2018-03-31 22:42:43 +0000

    credits return ['roles' : 'person']

---
+++ PythonApplication/PythonApplication/credits.py
@@ -0,0 +1,17 @@
+from soup_creator import create_soup
+
+
+def credits(href):
+    soup = create_soup(href)
+    role = []; people = []; dict = {}
+    for span in soup.find_all('span', {'class': 'role'}):
+       role.append(span.text)
+    for a in soup.find_all('a', {'class': 'rollover_link'}):
+       people.append(a.text.replace('\n', ''))
+    for i in range(len(role)):
+        dict[role[i]] = people[i]
+#TODO:Split roles since one person has more than one role sometimes 
+    return dict
+
+ 
+credits('https://www.discogs.com/Nikolija-101-Propušteni-Poziv/release/9843081')
