b5c6f802849e0ee0f7dd92c18d3c605083b7516b
Simonida <simkadic@uns.ac.rs>
2018-03-31 11:09:20 +0000

    Zapoceta konekcija

---
+++ PythonApplication/PythonApplication/PythonApplication.py
@@ -0,0 +1,11 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+  'host':'eestec.mysql.database.azure.com',
+  'user':'mysql@eestec',
+  'password':'Tesa2018',
+  'database':'mysql'
+}
+
+print("OK")
