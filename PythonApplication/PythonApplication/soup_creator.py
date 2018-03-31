237965c2890e54759951bdc4bd6e1e7bd2d4710b
johney9 <nikola.mihalek@gmail.com>
2018-03-31 18:48:26 +0000

    modified so requests have a user-agent, which increases throughput

--- PythonApplication/PythonApplication/soup_creator.py
+++ PythonApplication/PythonApplication/soup_creator.py
@@ -3,6 +3,6 @@ import requests
 
 
 def create_soup(url):
-    source_code = requests.get(url)
+    source_code = requests.get(url, headers = {'User-agent': 'SENa parser'})
     plain_text = source_code.text
     return BeautifulSoup(plain_text, "html.parser")
