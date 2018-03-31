18003cb63edfbd278a5f3d8fb9b8d85896e5d4bd
Simonida <simkadic@uns.ac.rs>
2018-03-31 23:17:36 +0000

    primeri

---
+++ PythonApplication/PythonApplication/callf.py
@@ -0,0 +1,35 @@
+# import connect_album_to_author
+# import insert_album
+# import insert_author
+# import insert_format
+# print(insert_format.func("test"))
+# print(connect_album_to_author.func("test", "test"))
+
+# import add_vocal_to_album
+# print(add_vocal_to_album.func("test", "test"))
+
+# import add_credits_to_album
+# print(add_credits_to_album.func("test", "test","test"))
+
+# import insert_role
+# print(insert_role.func("test"))
+
+
+# import insert_genre
+# print(insert_genre.func("test"))
+
+# import insert_style
+# print(insert_style.func("test"))
+
+# import insert_country
+# print(insert_country.func("test"))
+
+import add_song
+print(add_song.func("test", "test", 21, 22))
+
+
+# import insert_version
+# print(insert_version.func("test", "test",23,8,1994))
+
+# import connect_version_to_format
+# print(connect_version_to_format.func("test", 23, 8, 1994, "test"))
