f981955cf9511ac2f1b2e59b16b06112a07e0061
Simonida <simkadic@uns.ac.rs>
2018-03-31 11:21:00 +0000

    Povezivanje sa bazom.

--- PythonApplication/PythonApplication/PythonApplication.py
+++ PythonApplication/PythonApplication/PythonApplication.py
@@ -8,4 +8,23 @@ config = {
   'database':'mysql'
 }
 
-print("OK")
+# Construct connection string
+try:
+   conn = mysql.connector.connect(**config)
+   print("Connection established")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
+
+# Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+
+print("Ended Successfully")
