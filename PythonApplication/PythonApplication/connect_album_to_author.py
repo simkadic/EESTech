5078d561c9636f950b1d07ca230e85d9da7512eb
Simonida <simkadic@uns.ac.rs>
2018-03-31 19:28:52 +0000

    album - author

---
+++ PythonApplication/PythonApplication/connect_album_to_author.py
@@ -0,0 +1,36 @@
+def func(album,author):
+    import mysql.connector
+    from mysql.connector import errorcode
+
+    config = {
+        'host': 'eestec.mysql.database.azure.com',
+        'user': 'mysql@eestec',
+        'password': 'Tesa2018',
+        'database': 'mysql'
+    }
+
+    # Construct connection string
+    try:
+        conn = mysql.connector.connect(**config)
+        print("Connection established")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cursor = conn.cursor()
+        cursor.execute("use eestec;")
+        s = "INSERT INTO album_author (album_id,author_id) "
+        s += "SELECT album_id, author_id FROM author,album WHERE "
+        s += "author_name='%s\' " % (album)
+        s += "and album_name='%s\';" % (author)
+        print(s)
+        cursor.execute(s)
+        # Cleanup
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return "OK"
