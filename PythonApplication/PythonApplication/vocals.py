d99647994c17b1247de08a8b4775526c1ab3c943
johney9 <nikola.mihalek@gmail.com>
2018-03-31 20:16:49 +0000

    vocals returns vocalists

--- PythonApplication/PythonApplication/vocals.py
+++ PythonApplication/PythonApplication/vocals.py
@@ -3,12 +3,14 @@ from soup_creator import create_soup
 
 def vocals(href):
     soup = create_soup(href)
+    vocalists = []
+    
     
     for span in soup.find_all('span', {'class': 'role'}):
         if str("Vocals").lower() in span.text.lower():
             parent = span.parent
             for vocallist in parent.find_all('a', {'class': 'rollover_link'}):
-                print(vocallist.text)
-
+                vocalists.append(vocalist)
+    return vocalists
  
 vocals('https://www.discogs.com/Nikolija-101-Propušteni-Poziv/release/9843081')
